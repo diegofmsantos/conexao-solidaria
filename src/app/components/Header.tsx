61d3c594af1a9ae43edfafbaf606cddc1f19f7e2
diegofmsantos <diegofmsantos0@gmail.com>
2024-08-30 16:24:48 -0300

    add foto e ajuste header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,14 +14,14 @@ interface HeaderProps {
 
 export const Header = ({ scrollToSection, refs }: HeaderProps) => {
   return (
-    <header className="w-full h-32 flex justify-between items-center gap-8 fixed border-b border-b-[#CCC] bg-[#F6EFEE] z-50 md:px-24">
-      <div>
+    <header className="w-full h-32 flex items-center gap-8 fixed border-b border-b-[#CCC] bg-[#F6EFEE] z-50 md:px-24">
+      <div className="w-52">
         <Link href="/">
           <Image src="/assets/logo-header.png" width={200} height={200} alt="Logo" />
         </Link>
       </div>
-      <nav className="hidden md:flex">
-        <div className="flex justify-around items-center text-xl">
+      <nav className="hidden flex-1 justify-center md:flex">
+        <div className="flex justify-center items-center gap-20 text-xl">
           <button
             onClick={() => scrollToSection(refs.quemSomosRef)}
             className="hover:underline p-4 rounded-full"
--- src/app/components/QuemSomos.tsx
+++ src/app/components/QuemSomos.tsx
@@ -1,14 +1,23 @@
+import Image from "next/image";
+
 export const QuemSomos = () => {
     return (
         <div className="w-screen min-h-screen flex flex-col gap-10 justify-between items-center px-4 py-10 bg-[#57CC99] xl:flex-row xl:px-20 md:px-14">
-            <div className="max-w-[790px] min-h-[500px] flex justify-center items-center leading-snug text-xl text-white font-bold text-justify break-words hyphens-auto md:text-3xl">
+            <div className="max-w-[670px] min-h-[500px] flex justify-center items-baseline leading-snug text-xl text-white font-bold text-justify break-words hyphens-auto md:text-2xl xl:text-3xl">
                 Com o firme compromisso de conectar voluntários, organizações e beneficiários, a equipe do curso de
                 Programador de Sistemas do Senac-PE criou este projeto para oferecer maior visibilidade às ações solidárias.
                 Focamos na geolocalização para conectar os interessados com oportunidades próximas, assim facilitando na contribuição
                 e reduzindo barreiras logísticas e de conhecimento regional. Nosso objetivo é promover a solidariedade de forma eficiente e
                 impactante, aproximando aqueles que podem ajudar com aqueles que mais precisam.
             </div>
-            <div className="w-72 h-72 md:w-96 md:h-96 lg:w-[450px] lg:h-[450px] xl:w-[500px] xl:h-[500px] 2xl:w-[600px] 2xl:h-[600px] bg-slate-600 text-white flex justify-center items-center text-5xl">FOTO</div>
+            <div className="w-72 h-72 min-[375px]:w-[350px] min-[375px]:h-[350px] min-[425px]:w-[380px] min-[425px]:h-[380px] md:w-[550px] md:h-[550px] lg:w-[700px] lg:h-[700px] xl:w-[500px] xl:h-[500px] 2xl:w-[800px] 2xl:h-[600px]">
+                <Image 
+                    src="/assets/grupo.jpeg" 
+                    width={800} 
+                    height={800} 
+                    alt="Foto do grupo"
+                />
+            </div>
         </div>
     );
 };
