fb6def7afda5283e177156319d1310e999d14290
diegofmsantos <diegofmsantos0@gmail.com>
2024-08-30 17:26:42 -0300

    att

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface HeaderProps {
   scrollToSection: (ref: React.RefObject<HTMLDivElement>) => void;
@@ -13,6 +14,13 @@ interface HeaderProps {
 }
 
 export const Header = ({ scrollToSection, refs }: HeaderProps) => {
+  const [activeButton, setActiveButton] = useState<string | null>(null);
+
+  const handleClick = (section: string, ref: React.RefObject<HTMLDivElement>) => {
+    setActiveButton(section);
+    scrollToSection(ref);
+  };
+
   return (
     <header className="w-full h-32 flex items-center gap-8 fixed border-b border-b-[#CCC] bg-[#F6EFEE] z-50 md:px-24">
       <div className="w-52">
@@ -23,20 +31,20 @@ export const Header = ({ scrollToSection, refs }: HeaderProps) => {
       <nav className="hidden flex-1 justify-center md:flex">
         <div className="flex justify-center items-center gap-20 text-xl">
           <button
-            onClick={() => scrollToSection(refs.quemSomosRef)}
-            className="hover:underline p-4 rounded-full"
+            onClick={() => handleClick("quemSomos", refs.quemSomosRef)}
+            className={`p-4 rounded-full ${activeButton === "quemSomos" ? "underline" : ""}`}
           >
             Quem Somos
           </button>
           <button
-            onClick={() => scrollToSection(refs.doarRef)}
-            className="hover:underline p-4 rounded-full"
+            onClick={() => handleClick("doar", refs.doarRef)}
+            className={`p-4 rounded-full ${activeButton === "doar" ? "underline" : ""}`}
           >
             Doar
           </button>
           <button
-            onClick={() => scrollToSection(refs.contatoRef)}
-            className="hover:underline p-4 rounded-full"
+            onClick={() => handleClick("contato", refs.contatoRef)}
+            className={`p-4 rounded-full ${activeButton === "contato" ? "underline" : ""}`}
           >
             Contato
           </button>
